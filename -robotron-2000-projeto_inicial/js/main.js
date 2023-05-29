ef4423d02d4352d373a08688a8ccde0b098a83a1
RodYam <rodrigo.yoshio.yamamoto@gmail.com>
2023-05-29 08:27:51 -0300

    Update main.js

--- -robotron-2000-projeto_inicial/js/main.js
+++ -robotron-2000-projeto_inicial/js/main.js
@@ -1,4 +1,7 @@
-const robotron = document.querySelector("#robotron")
+const robotron = document.querySelector("#robotron");
+
+
+// As novas versões do JavaScript a partir do ES6 ainda tem uma forma diferente de declaramos uma função anônima. No lugar de usarmos function(), nós abrimos parênteses e colocamos uma seta formada por um sinal de igual e um maior => (arrow function).
 
 robotron.addEventListener("click", (evento) => {
     console.log(evento)
@@ -9,4 +12,4 @@ function dizOi(nome) {
     console.log("Bem-vindo(a) ao Robotron 2000.")
 }
 
-dizOi("Dakota");
+dizOi("Dakota"); // Queremos que todas as vezes que a página for carregada, a função dizOi seja executada. Para isso, nós chamamos esse função colocando o nome dela e abrindo e fechando parênteses, assim a função será executada no momento exato em que ela for lida.
